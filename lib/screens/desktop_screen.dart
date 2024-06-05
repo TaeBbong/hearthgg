f4ad66f7ad9c51af4acc37df510e9b08bd047c7a
TaeBbong <mok05289@korea.ac.kr>
2024-06-06 00:28:10 +0900

    wip: adding season selector

--- lib/screens/desktop_screen.dart
+++ lib/screens/desktop_screen.dart
@@ -32,81 +32,110 @@ class DesktopScreen extends GetView<HomeController> {
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(20.0),
-          ),
-          ModeIndicator(),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.4,
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: DropdownButtonHideUnderline(
-                    child: DropdownButtonFormField<AreaLabel>(
-                      value: controller.searchParams['area'],
-                      items: AreaLabel.values
-                          .map<DropdownMenuItem<AreaLabel>>((AreaLabel value) {
-                        return DropdownMenuItem<AreaLabel>(
-                          value: value,
-                          child: Text(
-                            value.text,
-                            style: Theme.of(context).textTheme.bodyMedium,
+      body: GetBuilder<HomeController>(builder: (controller) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(20.0),
+            ),
+            const ModeIndicator(),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.4,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButtonFormField<AreaLabel>(
+                        value: controller.searchParams['area'],
+                        items: AreaLabel.values
+                            .map<DropdownMenuItem<AreaLabel>>(
+                                (AreaLabel value) {
+                          return DropdownMenuItem<AreaLabel>(
+                            value: value,
+                            child: Text(
+                              value.text,
+                              style: Theme.of(context).textTheme.bodyMedium,
+                            ),
+                          );
+                        }).toList(),
+                        onChanged: (area) => controller.updateArea(area!),
+                        isExpanded: true,
+                        decoration: const InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.grey, width: 1),
                           ),
-                        );
-                      }).toList(),
-                      onChanged: (area) => controller.updateArea(area!),
-                      isExpanded: true,
-                      decoration: const InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey, width: 1),
+                          border: OutlineInputBorder(),
+                          filled: false,
                         ),
-                        border: OutlineInputBorder(),
-                        filled: false,
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  flex: 2,
-                  child: TextFormField(
-                    controller: controller.battleTagController,
-                    onFieldSubmitted: (_) async =>
-                        await controller.performSearch(),
-                    decoration: const InputDecoration(
-                      hintText: '배틀태그를 입력하세요. (ex: Flurry)',
-                      border: OutlineInputBorder(),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButtonFormField<AreaLabel>(
+                        value: controller.searchParams['area'],
+                        items: AreaLabel.values
+                            .map<DropdownMenuItem<AreaLabel>>(
+                                (AreaLabel value) {
+                          return DropdownMenuItem<AreaLabel>(
+                            value: value,
+                            child: Text(
+                              value.text,
+                              style: Theme.of(context).textTheme.bodyMedium,
+                            ),
+                          );
+                        }).toList(),
+                        onChanged: (area) => controller.updateArea(area!),
+                        isExpanded: true,
+                        decoration: const InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.grey, width: 1),
+                          ),
+                          border: OutlineInputBorder(),
+                          filled: false,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.search),
-                  onPressed: () async => await controller.performSearch(),
-                ),
-              ],
+                  const SizedBox(width: 10),
+                  Expanded(
+                    flex: 4,
+                    child: TextFormField(
+                      controller: controller.battleTagController,
+                      onFieldSubmitted: (_) async =>
+                          await controller.performSearch(),
+                      decoration: const InputDecoration(
+                        hintText: '배틀태그를 입력하세요. (ex: Flurry)',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.search),
+                    onPressed: () async => await controller.performSearch(),
+                  ),
+                ],
+              ),
             ),
-          ),
-          GetBuilder<HomeController>(
-            builder: (controller) {
-              return controller.isSearching
-                  ? const LoadingIndicator()
-                  : controller.isResult
-                      ? SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.41,
-                          child: RankCard(
-                              rankData:
-                                  RankData.fromJson(controller.searchResult)),
-                        )
-                      : Container();
-            },
-          ),
-          Expanded(child: Container()),
-          Footer(),
-        ],
-      ),
+            controller.isSearching
+                ? const LoadingIndicator()
+                : controller.isResult
+                    ? SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.41,
+                        child: RankCard(
+                            rankData:
+                                RankData.fromJson(controller.searchResult)),
+                      )
+                    : Container(),
+            Expanded(child: Container()),
+            Footer(),
+          ],
+        );
+      }),
     );
   }
 }
